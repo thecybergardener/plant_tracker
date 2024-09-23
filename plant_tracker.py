c3b4f23fe76a3d3546b3ad9824ce9d191ce9c593
Plant Daddy <info@0xplantdaddy.cc>
2024-09-23 16:29:04 -0400

    created main Tk window

---
+++ plant_tracker.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import filedialog
+from openpyxl import load_workbook
+from datetime import datetime
+import os
+import shutil
+
+# Create the main window for data entry
+root = tk.Tk()
+root.title("PlantDaddy's Plant Tracker")
+
+# Run Main application
+root.mainloop()
